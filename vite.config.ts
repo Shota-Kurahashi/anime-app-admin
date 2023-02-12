1e34fa8a3883721ebcfea13ac807801bbd0467ba
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-02-12 14:10:52 +0900

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "dist",
   root: "./src",
   publicDir: "../public",
   build: {
     outDir: "../dist",
-    emptyOutDir: true,
   },
   server: {
     port: 3000,
